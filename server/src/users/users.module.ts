7baa804d6c5ee0121a2d4746b9c9be691e49252e
omowonuola <oyedmola@gmail.com>
2023-03-31 23:40:16 +0100

    adds user signup repository and contoller api

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UserService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './users.repository';
+import { JwtStrategy } from './jwt-passport';
+import { UserEntity } from './user.entity';
 
 @Module({
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      // imports: [ConfigModule],
+      // secret: process.env.JWT_SECRET,
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: {
+        expiresIn: '365d',
+      },
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
+  providers: [UserService, UserRepository, JwtStrategy],
   controllers: [UsersController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class UsersModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -3,12 +3,10 @@ import { UserCredentialsDto } from './dto/user-credentials.dto';
 import { UserRepository } from './users.repository';
 
 @Injectable()
-export class AuthService {
+export class UserService {
   constructor(private userRepository: UserRepository) {}
 
   async signUp(userCredentialsDto: UserCredentialsDto): Promise<any> {
     return this.userRepository.createUser(userCredentialsDto);
   }
-
-
 }
