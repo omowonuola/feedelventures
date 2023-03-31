ee491f136ed73c2b59b6437bfe25701afd84298e
omowonuola <oyedmola@gmail.com>
2023-03-31 23:14:25 +0100

    adds user dto file

---
+++ server/src/users/users.repository.ts
@@ -0,0 +1,10 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './user.entity';
+import { AuthCredentialsDto } from './dto/user-credentials.dto';
